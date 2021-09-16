7b874ffa09658fb91c419c6cfec9ab613e401bbd
feulix <felix@vangeuns.nl>
2021-09-16 13:50:31 +0200

    Update Open all links from mail.missiveapp.com in app.user.js
    Fix for Safari - window was closed while user was asked if Safari was allowed to open Missive. Ugly workaround: close window after a few seconds.

--- Open all links from mail.missiveapp.com in app.user.js
+++ Open all links from mail.missiveapp.com in app.user.js
@@ -1,12 +1,16 @@
 // ==UserScript==
 // @name     	Open all links from mail.missiveapp.com in app
-// @version  	1.2
+// @version  	1.21
 // @grant    	window.close
 // @updateURL	https://github.com/feulix/userscript-missiveapp-redirect/raw/main/Open%20all%20links%20from%20mail.missiveapp.com%20in%20app.user.js
 // @downloadURL	https://github.com/feulix/userscript-missiveapp-redirect/raw/main/Open%20all%20links%20from%20mail.missiveapp.com%20in%20app.user.js
+// @require        https://raw.github.com/odyniec/MonkeyConfig/master/monkeyconfig.js
 // @include     https://mail.missiveapp.com/#*
 // @run-at 	document-end
 // ==/UserScript==
+
+//Amount of seconds to wait for user input when redirecting
+var close_timeout = 4;
 
 console.log('window.location.href', window.location.href);
 console.log('document.location.hash', document.location.hash);
@@ -28,7 +32,20 @@ console.log('returnTo','missive://mail.missiveapp.com/'+returnTo);
 
 //alert('Greasemonkey script is redirecting you...');
 //if (confirm("Allow Greasemonkey script to redirect you to app?")) {
-	window.location = 'missive://mail.missiveapp.com/'+returnTo;
-	window.close();
+window.location = 'missive://mail.missiveapp.com/'+returnTo
+
+var timeleft = close_timeout;
+var downloadTimer = setInterval(function(){
+    if(timeleft <= 0){
+        clearInterval(downloadTimer);
+    }
+    document.body.innerHTML = "Closing window in " + timeleft + " seconds...";
+    if(timeleft == 0) {
+        window.close();
+    }
+    timeleft -= 1;
+}, 1000);
+
 //} 
+
 
